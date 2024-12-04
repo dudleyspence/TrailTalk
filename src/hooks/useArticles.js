ea1b10acd4eb71e689175aa6cde5f550528cc6e4
Dudley Spence <dudleyspence5@gmail.com>
2024-12-04 21:32:27 +0100

    useArticle hook complete

---
+++ src/hooks/useArticles.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const useArticles = ({
+  topic,
+  sortBy,
+  order,
+  pageNo,
+  articlesPerPage,
+  username,
+}) => {
+  const [articles, setArticles] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchArticles(
+      topic,
+      sortBy,
+      order,
+      pageNo,
+      articlesPerPage,
+      (username = null)
+    )
+      .then(({ data }) => {
+        setTotal(data.total);
+        setArticles(data.articles);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  return { articles, total, loading, error };
+};
+
+export default useArticles;
